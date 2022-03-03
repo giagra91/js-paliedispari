0c16d2273b146a9b38d600450b028fc8ae6f6a2a
giagra91 <giagra91@gmail.com>
2022-03-03 16:42:34 +0100

    modifiche condizioni gioco pari e dispari

--- js/main.js
+++ js/main.js
@@ -30,32 +30,41 @@ console.log(palyndromeCheck(userWord))
 const userChoice = prompt(`Scegli tra pari e dispari.`).toLowerCase().trim();
 const userNumber = parseInt(prompt(`Inserisci un numero tra 1 e 5`));
 
+if (userNumber > 5 || userNumber < 1 || isNaN(userNumber)){
+    alert(`Hai inserito un numero non valido`);
+    userNumber = parseInt(prompt(`Inserisci un numero tra 1 e 5`));
+}
 
+// Funzione per numero randomico tra due valori
 function randomNumber(minimumValue, maximumValue){
     return (Math.floor(Math.random()* (( maximumValue + 1 ) - minimumValue ) + minimumValue));
 }
 
 const pcNumber = randomNumber(1,5);
 
-const sumNumber = parseInt( userNumber + pcNumber);
+const sumNumber = parseInt( userNumber + pcNumber );
 
 console.log(pcNumber)
 console.log(sumNumber)
 
-function checkSum(number){
+function checkSum(num){
 
-    if (number % 2 == 0){
-        console.log (`pari`)
+    if (num % 2 == 0){
+        console.log (`pari`);
+        return "pari";
     } else {
-        console.log(`dispari`)
+        console.log(`dispari`);
+        return "dispari";
     }
 }
 
-// const gameSum = checkSum(sumNumber);
-console.log(checkSum(sumNumber))
+const gameSum = checkSum(sumNumber);
+const oddOrEvenoOutput = document.getElementById(`oddoreven-output`);
 
-if (userChoice == "pari"){
+if (gameSum == userChoice){
     console.log(`Hai vinto!`);
+    oddOrEvenoOutput.innerHTML=`La somma è ${sumNumber}, quindi ${gameSum}, hai vinto!`
 } else {
-    console.log(`Ha vinto il computer`)
+    console.log(`Ha vinto il computer!`);
+    oddOrEvenoOutput.innerHTML=`La somma è ${sumNumber}, quindi ${gameSum}, ha vinto il computer!`
 }
